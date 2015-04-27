09f71684964eb8df70c255da6dcf7be8292e9323
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-04-27 20:47:03 +0200

    Initialize correctly Topics, Catalog and LayersConfig from WMS server

--- initializedb.py
+++ initializedb.py
@@ -2,6 +2,9 @@ import os
 import sys
 
 from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+
+from owslib.wms import WebMapService
 
 from pyramid.paster import (
     get_appsettings,
@@ -12,8 +15,13 @@ from pyramid.scripts.common import parse_vars
 
 from chsdi.models.bod import (
     Base,
+    Topics,
+    LayersConfig,
+    Catalog,
     )
-from chsdi.models import dbs
+
+
+TOPIC = 'cwdev_geojb'
 
 
 def usage(argv):
@@ -30,9 +38,61 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
-    for db in dbs:
-        engine = engine_from_config(settings, 'sqlalchemy.{}.'.format(db))
-        Base.metadata.create_all(engine)
+    engine = engine_from_config(settings, 'sqlalchemy.bod.')
+    initialize_bod(engine)
+
+
+def initialize_bod(engine):
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    add_topics(session)
+    add_layers_config(session)
+    add_catalog(session)
+
+
+def add_topics(session):
+    geojb = Topics(id=TOPIC, orderKey=0, availableLangs='fr', selectedLayers=[],
+                        backgroundLayers=['COUVERTUREDUSOL'], showCatalog=True, staging='')
+    session.add(geojb)
+    session.commit()
+
+
+def add_layers_config(session):
+    wms_url = 'http://mapserver.local/wms/geojbwms'
+    wms_version = '1.1.0'
+    wms = WebMapService(wms_url, version=wms_version)
+    for layer_name, layer in wms.contents.items():
+        styles = layer.styles
+        try:
+            legend = styles['default']['legend']
+        except KeyError:
+            legend = ''
+        has_legend = bool(legend)
+        format = 'PNG'
+        type = 'wms'
+        opacity = layer.opaque
+        queryable = bool(layer.queryable)
+        layer_row = LayersConfig(layerBodId=layer_name, background=False, hasLegend=has_legend,
+                            format=format, type=type, opacity=opacity, queryable=queryable,
+                            serverLayerName=layer_name, wmsLayers=layer_name, wmsUrl=wms_url)
+        if layer_name == 'COUVERTUREDUSOL':
+            layer_row.background = True
+        session.add(layer_row)
+    session.commit()
+
+
+def add_catalog(session):
+    root = Catalog(topic=TOPIC, category='root', depth=0, path='root')
+    session.add(root)
+    for layer in session.query(LayersConfig):
+        name = layer.layerBodId
+        catalog_entry = Catalog(parentId=1, topic=TOPIC, category='layer', layerBodId=name,
+                                    nameDe=name, nameFr=name,
+                                    nameIt=name, nameRm=name, nameEn=name, depth=1, path='root/' + name)
+        session.add(catalog_entry)
+    session.commit()
 
 
 if __name__ == '__main__':

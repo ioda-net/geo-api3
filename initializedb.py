507e6f96605e8ce5976e231318a51194ca239ab9
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-04-27 16:33:04 +0200

    Add script to initalize the 'bod' database
    - Create the required tables
    - Inspired by http://docs.pylonsproject.org/projects/pyramid/en/latest/tutorials/wiki2/definingmodels.html#changing-scripts-initializedb-py

---
+++ initializedb.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+from pyramid.scripts.common import parse_vars
+
+from chsdi.models.bod import (
+    Base,
+    )
+from chsdi.models import dbs
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri> [var=value]\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv=sys.argv):
+    if len(argv) < 2:
+        usage(argv)
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri, options=options)
+    for db in dbs:
+        engine = engine_from_config(settings, 'sqlalchemy.{}.'.format(db))
+        Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    main()

9624cab5a8ed60f9714d2c98a3a06c71576e7068
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-05 15:48:10 +0200

    Remove the downloads from pypi.camptocamp.net
    Not needed.

--- deploy.py
+++ deploy.py
@@ -34,10 +34,7 @@ def main(args):
 
 
 def init():
-    cmd = ['python', 'bootstrap.py', '--version', '1.5.2', '--distribute',
-        '--download-base', 'http://pypi.camptocamp.net/distribute-0.6.22_fix-issue-227/',
-        '--setup-source', 'http://pypi.camptocamp.net/distribute-0.6.22_fix-issue-227/distribute_setup.py'
-    ]
+    cmd = ['python', 'bootstrap.py', '--version', '1.5.2']
     subprocess.call(cmd)
 
 

0bc741d56aab4bdf2862c7069e9a5bfd83189261
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-11 12:08:08 +0200

    Create the `remove_accent` pg function when initializing the database
    See #4

--- initializedb.py
+++ initializedb.py
@@ -60,10 +60,43 @@ def initialize_bod(engine):
     config = configparser.ConfigParser()
     config.read('buildout_config.cfg')
 
+    init_functions(session)
     add_topics(session)
     add_layers_config(session, config)
     add_catalog(session)
     session.commit()
+
+
+def init_functions(session):
+    remove_accent = u"""
+-- custom postgres sql function remove_accents
+CREATE OR REPLACE FUNCTION public.remove_accents(string character varying)
+  RETURNS character varying AS
+$BODY$
+    DECLARE
+        res varchar;
+    BEGIN
+        res := replace(string, 'ü', 'ue');
+        res := replace(res, 'Ü', 'ue');
+        res := replace(res, 'ä', 'ae');
+        res := replace(res, 'Ä', 'ae');
+        res := replace(res, 'ö', 'oe');
+        res := replace(res, 'Ö', 'oe');
+        res := replace(res, '(', '_');
+        res := replace(res, ')', '_');
+        res:= translate(res, 'àáâÀÁÂ', 'aaaaaa');
+        res:= translate(res, 'èéêëÈÉÊË', 'eeeeeeee');
+        res:= translate(res, 'ìíîïÌÍÎÏ', 'iiiiiiii');
+        res:= translate(res, 'òóôÒÓÔ', 'oooooo');
+        res:= translate(res, 'ùúûÙÚÛ', 'uuuuuu');
+        res:= translate(res, 'ç', 'c');
+        RETURN trim(lower(res));
+    END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+"""
+    session.execute(remove_accent)
 
 
 def add_topics(session):

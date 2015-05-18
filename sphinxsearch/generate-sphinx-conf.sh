dbf1f1009f366a06590a541aab3eb6d84b1d44c4
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-18 17:30:48 +0200

    Sphinx configuration is generated from templates

--- sphinxsearch/generate-sphinx-conf.sh
+++ sphinxsearch/generate-sphinx-conf.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+error_exit() {
+    echo "$1" >&2
+    exit "${2:-1}"
+}
+
+cd ..
+./deploy.py > /dev/null 2>&1 || error_exit "Failed to generate template"
+cd "sphinxsearch"
+
 cat sphinx-base.conf db.conf search*.conf > sphinx.conf
 
 if ! indextool --checkconfig -c sphinx.conf > /dev/null 2>&1; then

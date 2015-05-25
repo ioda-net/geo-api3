0a7f90fc6f163a6ede144990a5bbcd709f879bc5
Bruno Friedmann <bruno@ioda-net.ch>
2015-05-25 13:52:23 +0200

    Added sudo actions for copy sphinx.conf and restart searchd

--- sphinxsearch/generate-sphinx-conf.sh
+++ sphinxsearch/generate-sphinx-conf.sh
@@ -13,5 +13,13 @@ cat sphinx-base.conf db.conf search*.conf > sphinx.conf
 
 if ! indextool --checkconfig -c sphinx.conf > /dev/null 2>&1; then
     indextool --checkconfig -c sphinx.conf
-    exit 1
+#   If we have missing indexes we will never
+#   create the first configuration
+#    exit 1
 fi
+
+chmod 0640 sphinx.conf
+sudo cp -a sphinx.conf /etc/sphinx/sphinx.conf
+sudo chown sphinx:sphinx /etc/sphinx/sphinx.conf
+sudo systemctl restart searchd.service
+

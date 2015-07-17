7f5b611ccc9d182b0fcd395e2d30a1e9fc46d0f5
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-07-17 10:21:59 +0200

    Improve the communes endpoint
    - Always use 21781 as srid (will never get result with another one)
    - Improve return if no result

--- chsdi/views/communes.py
+++ chsdi/views/communes.py
@@ -2,6 +2,7 @@
 
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPBadRequest
+from sqlalchemy.orm.exc import NoResultFound
 
 from chsdi.models.utilities import Communes
 
@@ -10,11 +11,14 @@ from chsdi.models.utilities import Communes
 def communes(request):
     x = request.params.get('x')
     y = request.params.get('y')
-    srid = request.params.get('srid')
-    if x is None or y is None or srid is None:
-        raise HTTPBadRequest('You must provide the coordonates and their projection')
-    point = 'SRID={};POINT({} {})'.format(srid, x, y)
-    commune = request.db.query(Communes)\
-        .filter(Communes.the_geom.ST_Contains(point))\
-        .one()
-    return {'commune': commune.nom}
+    if not x or not y:
+        raise HTTPBadRequest('You must provide the coordonates.')
+    point = 'SRID={};POINT({} {})'.format(21781, x, y)
+    try:
+        commune = request.db.query(Communes)\
+            .filter(Communes.the_geom.ST_Contains(point))\
+            .one()
+    except NoResultFound:
+        return {}
+    else:
+        return {'commune': commune.nom}

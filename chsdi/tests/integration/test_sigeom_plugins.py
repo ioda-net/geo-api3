9e598484cb1318f02862629d3c8cac8776e01701
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-07-27 19:28:50 +0200

    Add tests for communes

---
+++ chsdi/tests/integration/test_sigeom_plugins.py
@@ -0,0 +1,19 @@
+from chsdi.tests.integration import TestsBase
+
+
+class TestCommunes(TestsBase):
+    def setUp(self):
+        super().setUp()
+
+    def test_no_coords(self):
+        self.testapp.get('/communes', status=400)
+        self.testapp.get('/communes?x=0', status=400)
+        self.testapp.get('/communes?y=0', status=400)
+
+    def test_no_result(self):
+        resp = self.testapp.get('/communes?x=-1&y=-1', status=200)
+        self.failUnless(resp.json == {})
+
+    def test_commune(self):
+        resp = self.testapp.get('/communes?x=594171&y=236290', status=200)
+        self.failUnless(resp.json['commune'] == 'Moutier')

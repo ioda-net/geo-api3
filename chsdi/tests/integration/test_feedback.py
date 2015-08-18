8cb37b5b928b5b9fdaf7b2c8b42abc8f40abf1c6
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-08-18 16:31:58 +0200

    Refactor feedback
    - Move into a class
    
    Close #30

---
+++ chsdi/tests/integration/test_feedback.py
@@ -0,0 +1,52 @@
+from collections import OrderedDict
+from chsdi.tests.integration import TestsBase
+
+
+class TestFeedback(TestsBase):
+    def setUp(self):
+        super().setUp()
+        self.payload = OrderedDict()
+        self.payload['email'] = 'vador@ioda.net'
+        self.payload['feedback'] = 'Ça ne fonctionne pas'
+        self.payload['ua'] = 'User Agent'
+        self.payload['permalink'] = 'http://geojb.ch'
+        self.empty_kml = ''
+        self.kml = '<kml></kml>'
+        self.file = ('attachment', 'test.txt', 'Cocuou'.encode('utf-8'))
+
+    def test_send_mail(self):
+        resp = self.testapp.post('/feedback', params=self.payload, status=200)
+        self.failUnless(resp.json['success'])
+
+    def test_send_mail_with_empty_kml(self):
+        self.payload['kml'] = self.empty_kml
+        resp = self.testapp.post(
+            '/feedback',
+            params=self.payload,
+            status=200)
+        self.failUnless(resp.json['success'])
+
+    def test_send_mail_with_kml(self):
+        self.payload['kml'] = self.kml
+        resp = self.testapp.post(
+            '/feedback',
+            params=self.payload,
+            status=200)
+        self.failUnless(resp.json['success'])
+
+    def test_send_mail_with_file(self):
+        resp = self.testapp.post(
+            '/feedback',
+            params=self.payload,
+            upload_files=[self.file],
+            status=200)
+        self.failUnless(resp.json['success'])
+
+    def test_send_mail_all(self):
+        self.payload['kml'] = self.kml
+        resp =  self.testapp.post(
+            '/feedback',
+            params=self.payload,
+            upload_files=[self.file],
+            status=200)
+        self.failUnless(resp.json['success'])
--- chsdi/views/feedback.py
+++ chsdi/views/feedback.py
@@ -1,110 +1,126 @@
 import cgi
 import json
 import datetime
-from pyramid.view import view_config
 
+from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPInternalServerError
 from smtplib import SMTPException
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
 
 
-@view_config(route_name='feedback', renderer='json', request_method='POST')
-def feedback(self, request):
-    defaultRecipient = request.registry.settings['feedback.default_recipient']
-    defaultSubject = request.registry.settings['feedback.default_subject']
+class Feedback:
+    def __init__(self, request):
+        self.request = request
+        self.default_recipient = \
+            request.registry.settings['feedback.default_recipient']
+        self.default_subject = \
+            request.registry.settings['feedback.default_subject']
+        self.mail_host_name = \
+            request.registry.settings['feedback.mail_host_name']
+        self.mail_host_port = \
+            request.registry.settings['feedback.mail_host_port']
 
-    def getParam(param, defaultValue):
-        val = request.params.get(param, defaultValue)
-        val = val if val != '' else defaultValue
-        return val
+    @view_config(route_name='feedback', renderer='json', request_method='POST')
+    def feedback(self):
+        self.parse_params()
+        msg = self.prepare_message()
+        success = self.send(msg)
+        return {'success': success}
 
-    def mail(to, subject, text, attachement, kml, kmlfilename, jsonToAttach):
-        from email.mime.multipart import MIMEMultipart
-        from email.mime.base import MIMEBase
-        from email.mime.text import MIMEText
-        from email import encoders
-#        import unicodedata
-        import smtplib
+    def parse_params(self):
+        ua = self.get_param('ua', 'no user-agent found')
+        permalink = self.get_param('permalink', 'No permalink provided')
+        feedback = self.get_param('feedback', 'No feedback provided')
+        self.user_email = self.get_param('email', None)
+        self.subject = self.get_param('subject', None)
+        text_format = {
+            'user': self.user_email,
+            'feedback': feedback,
+            'permalink': permalink,
+            'ua': ua,
+        }
+        self.text = '''{user} sent a feedback:
+\t{feedback}.
+Permalink: {permalink}.
 
+User-Agent: {ua}'''.format(**text_format)
+        self.attachment = self.get_param('attachment', None)
+        self.kml = self.get_param('kml', None)
+        now = datetime.datetime.now()
+        time_id = now.strftime('%Y%m%d%H%M%S%f')[0:16]
+        self.default_subject += ' ID: {}'.format(time_id)
+        self.kmlfilename = 'Drawing-{}.kml'.format(time_id)
+        self.jsonfilename = 'Meta-{}.json'.format(time_id)
+        attachfilename = ''
+        if isinstance(self.attachment, cgi.FieldStorage):
+            attachfilename = self.attachment.filename
+
+        self.metadata = json.dumps({
+            'emailAddress': self.user_email,
+            'body': feedback,
+            'permalink': permalink,
+            'kml': self.kmlfilename if (self.kml is not None and self.kml != '') else '',
+            'attachment': attachfilename,
+            'userAgent': ua,
+            'ID': time_id
+        })
+
+    def get_param(self, param, default_value):
+        val = self.request.params.get(param, default_value)
+        # Note: attachment (cgi.FieldStorage) cannot be converted to bool
+        return val if val != '' else default_value
+
+    def prepare_message(self):
         msg = MIMEMultipart()
+        msg['To'] = self.default_recipient
+        msg['From'] = self.user_email if self.user_email else self.default_recipient
+        msg['Subject'] = self.subject if self.subject else self.default_subject
+        msg.attach(MIMEText(self.text, _charset='utf-8'))
+        self.add_attachment(msg, self.attachment)
+        self.add_attachment(
+            msg,
+            self.metadata,
+            mime_base=('application', 'json'),
+            filename=self.jsonfilename)
+        self.add_attachment(
+            msg,
+            self.kml,
+            mime_base=('application', 'vnd.google-earth.kml+xml'),
+            filename=self.kmlfilename)
 
-        msg['To'] = to
-        msg['From'] = to
-        msg['Subject'] = subject
+        return msg
 
-        msg.attach(MIMEText(text, _charset='utf-8'))
-        # Attach meta information
-        part = MIMEBase('application', 'json')
-        part.set_payload(json.dumps(jsonToAttach))
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename=' + jsonfilename)
-        msg.attach(part)
-
-        # Attach file if there
-        if isinstance(attachement, cgi.FieldStorage):
-            types = attachement.type.split('/')
-            if len(types) != 2:
+    def add_attachment(self, msg, attachment, mime_base=('application', 'json'), filename=''):
+        payload = None
+        if isinstance(attachment, cgi.FieldStorage):
+            types = attachment.type.split('/')
+            if len(types) != 2:  # pragma: no cover
                 raise HTTPInternalServerError('File type could not be determined')
             part = MIMEBase(types[0], types[1])
-            filePart = attachement.file.read()
-            part.set_payload(filePart)
+            payload = attachment.file.read()
+            filename = attachment.filename
+        elif attachment:
+            part = MIMEBase(mime_base[0], mime_base[1])
+            payload = attachment
+
+        if payload is not None:
+            part.set_payload(payload)
             encoders.encode_base64(part)
-            part.add_header('Content-Disposition', 'attachment; filename="%s"' % attachement.filename)
+            part.add_header('Content-Disposition', 'attachment; filename={}'.format(filename))
             msg.attach(part)
 
-        # Attach kml if there
-        if kml is not None and kml is not '':
-            part = MIMEBase('application', 'vnd.google-earth.kml+xml')
-            part.set_payload(kml)
-            encoders.encode_base64(part)
-            part.add_header('Content-Disposition', 'attachment; filename=' + kmlfilename)
-            msg.attach(part)
-
-        mailServer = smtplib.SMTP(
-            request.registry.settings['feedback.mail_host_name'],
-            request.registry.settings['feedback.mail_host_port'])
-        err = mailServer.send_message(msg)
-        if err:
-            print(err)
-        mailServer.quit()
-
-    ua = getParam('ua', 'no user-agent found')
-    permalink = getParam('permalink', 'No permalink provided')
-    feedback = getParam('feedback', 'No feedback provided')
-    email = getParam('email', 'Anonymous')
-    text = u'%s sent a feedback:\n %s. \nPermalink: %s. \n\nUser-Agent: %s'
-    attachement = getParam('attachement', None)
-    kml = getParam('kml', None)
-    now = datetime.datetime.now()
-    timeID = now.strftime('%Y%m%d%H%M%S%f')[0:16]
-    defaultSubject = defaultSubject + ' ID : ' + timeID
-    kmlfilename = 'Drawing-' + timeID + '.kml'
-    jsonfilename = 'Meta-' + timeID + '.json'
-    attachfilename = ''
-    if isinstance(attachement, cgi.FieldStorage):
-        attachfilename = attachement.filename
-
-    jsonAtt = {
-        'emailAddress': email,
-        'body': feedback,
-        'permalink': permalink,
-        'kml': kmlfilename if (kml is not None and kml is not '') else '',
-        'attachement': attachfilename,
-        'userAgent': ua,
-        'ID': timeID
-    }
-
-    try:
-        mail(
-            defaultRecipient,
-            defaultSubject,
-            text % (email, feedback, permalink, ua),
-            attachement,
-            kml,
-            kmlfilename,
-            jsonAtt
-        )
-    except SMTPException as e:
-        print(e)
-        raise HTTPInternalServerError()
-
-    return {'success': True}
+    def send(self, msg):
+        try:
+            with SMTP(host=self.mail_host_name, port=self.mail_host_port) as smtp:
+                    smtp.send_message(msg)
+        except SMTPException as e:  # pragma: no cover
+            print('*** SMTPException', e)
+            raise HTTPInternalServerError()
+        except Exception:  # pragma: no cover
+            return False
+        else:
+            return True

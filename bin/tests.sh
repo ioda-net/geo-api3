c4b6aadf65e9a505d18459c93bbea4c4432d564e
Julien Enselme <julien.enselme@ioda-net.ch>
2015-09-08 15:12:49 +0200

    Can use test script to launch multiple tests

--- bin/tests.sh
+++ bin/tests.sh
@@ -4,13 +4,4 @@ NOSE_CMD="./.venv/bin/nosetests"
 PYTHON_VERSION=$(python3 --version 2>&1 | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
 PYTHONPATH=".venv/lib/python${PYTHON_VERSION}/site-packages:/usr/lib64/python${PYTHON_VERSION}/site-packages"
 
-pwd
-if [[ -n "$2" ]]; then
-    if [[ "$2" == -* ]]; then
-        PYTHONPATH=$PYTHONPATH "${NOSE_CMD}" "$1" "$2"
-    else
-        PYTHONPATH=$PYTHONPATH "${NOSE_CMD}" "$1:$2"
-    fi
-else
-    PYTHONPATH=$PYTHONPATH "${NOSE_CMD}" "$1"
-fi
+PYTHONPATH=$PYTHONPATH "${NOSE_CMD}" "$@"

a7cee322094f4ae903f0040e29f7c1a1953b4939
Bruno Friedmann <bruno@ioda-net.ch>
2016-08-10 09:49:04 +0200

    Add non arch usr/lib

--- config/config.dist.sh
+++ config/config.dist.sh
@@ -3,7 +3,7 @@
 # We need GDAL which is hard to install in a venv, modify PYTHONPATH to use the
 # system wide version.
 PYTHON_VERSION=$(python3 --version 2>&1 | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
-export PYTHONPATH=".venv/lib/python${PYTHON_VERSION}/site-packages:/usr/lib64/python${PYTHON_VERSION}/site-packages:$(pwd)"
+export PYTHONPATH=".venv/lib/python${PYTHON_VERSION}/site-packages:/usr/lib64/python${PYTHON_VERSION}/site-packages:/usr/lib/python${PYTHON_VERSION}/site-packages:$(pwd)"
 
 FLAKE8="$(pwd)/.venv/bin/flake8"
 JINJA2="$(pwd)/.venv/bin/jinja2"

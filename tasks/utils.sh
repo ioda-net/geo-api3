420f59e300830a40bf0153f892419ec45431e17a
Julien Enselme <julien.enselme@ioda-net.ch>
2017-01-13 17:41:21 +0100

    Allow tasks ini-files and wsgi-files to be launched from outside a git repository

--- tasks/utils.sh
+++ tasks/utils.sh
@@ -28,12 +28,19 @@ HELP['ini-files']="manuel ini-files
 Regenerate the ini files for pyramid from the configuration."
 function ini-files {
     declare -a ini_files=(production.ini development.ini uwsgi.ini)
-    local current_branch=$(git rev-parse --abbrev-ref HEAD)
+    local current_branch=$(_get-current-branch)
     for file in ${ini_files[@]}; do
         python3 tasks/config.py "${current_branch}" | "${JINJA2}" --format json \
                     -Dinstall_directory="$(pwd)" \
                     "${file}.jinja2" > "${file}"
     done
+}
+
+
+function _get-current-branch {
+    # Fallback to master if the repo is not a git repository
+    local branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null || echo master)
+    echo "${branch}"
 }
 
 
@@ -43,7 +50,7 @@ Regenetare apache's wsgi-files from the configuration."
 function wsgi-files {
     ini-files
     declare -a wsgi_files=(production.wsgi development.wsgi)
-    local current_branch=$(git rev-parse --abbrev-ref HEAD)
+    local current_branch=$(_get-current-branch)
     mkdir -p parts/wsgi
     for file in ${wsgi_files[@]}; do
         python3 tasks/config.py "${current_branch}" | "${JINJA2}" --format json \
